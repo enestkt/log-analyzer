cc593215480d90243689d320f3a292e8fc5d5097
enestkt <121498839+enestkt@users.noreply.github.com>
2026-09-16 17:18:12 +0300

    fix(gui): uzun mesajlarda vurgulanan eslesme gorunur hale getirildi
    - Eslesme mesajin hucreye sigmayan kisminda kaldiginda metin bastan
      kirpilip eslesmenin biraz oncesinden gosterildi, basina ve sonuna uc
      nokta eklendi
    - Kalin yazilan eslesen karakterlerin genisligi dogru fontla olculdu
    - HTML'e cevrilirken ard arda gelen bosluklarin tek bosluga inmesi onlendi

--- src/ui/HighlightDelegate.cpp
+++ src/ui/HighlightDelegate.cpp
@@ -1,6 +1,7 @@
 #include "HighlightDelegate.h"
 
 #include <QApplication>
+#include <QFontMetricsF>
 #include <QPainter>
 #include <QTextDocument>
 
@@ -37,20 +38,84 @@ void HighlightDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opt
     background.text.clear();
     style->drawControl(QStyle::CE_ItemViewItem, &background, painter, opt.widget);
 
-    // 2) Metni HTML olarak kur: eslesen araliklar mavi + kalin <span> icinde,
-    //    geri kalan tablodaki normal metin renginde. toHtmlEscaped, log
-    //    mesajindaki '<' '>' gibi karakterlerin HTML sanilmasini onler.
-    QString html = QStringLiteral("<span style=\"color:#334155;\">");
-    qsizetype cursor = 0;
+    const QRect textRect = style->subElementRect(QStyle::SE_ItemViewItemText, &opt, opt.widget);
+    const qreal available = textRect.width();
+
+    // 2) Metnin hangi kismi gorunecek? Eslesen karakterler kalin cizildigi icin
+    //    genislik karakter karakter, dogru fontla olculur.
+    QFont boldFont = opt.font;
+    boldFont.setBold(true);
+    const QFontMetricsF normalMetrics(opt.font);
+    const QFontMetricsF boldMetrics(boldFont);
+
+    QVector<bool> inMatch(text.size(), false);
     for (const auto &span : spans) {
-        html += text.mid(cursor, span.first - cursor).toHtmlEscaped();
+        for (qsizetype i = span.first; i < span.first + span.second; ++i)
+            inMatch[i] = true;
+    }
+    const auto charWidth = [&](qsizetype i) {
+        return inMatch[i] ? boldMetrics.horizontalAdvance(text.at(i))
+                          : normalMetrics.horizontalAdvance(text.at(i));
+    };
+    const QString ellipsis = QStringLiteral("…");
+    const qreal ellipsisWidth = normalMetrics.horizontalAdvance(ellipsis);
+
+    //    Ilk eslesme hucreye sigmiyorsa (uzun mesajin sonundaysa) metni bastan
+    //    kirp: eslesmeden once alanin yaklasik ucte biri kadar baglam birak.
+    //    Boylece kullanici satirin neden geldigini her zaman gorur.
+    qsizetype start = 0;
+    const qsizetype firstMatchStart = spans.first().first;
+    const qsizetype firstMatchEnd = firstMatchStart + spans.first().second;
+    qreal widthToFirstMatchEnd = 0;
+    for (qsizetype i = 0; i < firstMatchEnd; ++i)
+        widthToFirstMatchEnd += charWidth(i);
+    if (widthToFirstMatchEnd > available) {
+        start = firstMatchStart;
+        qreal context = ellipsisWidth;
+        while (start > 0 && context + charWidth(start - 1) <= available / 3) {
+            --start;
+            context += charWidth(start);
+        }
+    }
+
+    //    Sigan kadar karakter al; tasan kisim varsa sona "..." birak.
+    qreal used = start > 0 ? ellipsisWidth : 0;
+    qsizetype end = start;
+    while (end < text.size() && used + charWidth(end) <= available) {
+        used += charWidth(end);
+        ++end;
+    }
+    const bool cutEnd = end < text.size();
+    if (cutEnd) {
+        while (end > start && used + ellipsisWidth > available) {
+            --end;
+            used -= charWidth(end);
+        }
+    }
+
+    // 3) Gorunen parcayi HTML olarak kur: eslesen araliklar sari zemin + kalin,
+    //    geri kalan tablodaki normal metin renginde. toHtmlEscaped, log
+    //    mesajindaki '<' '>' gibi karakterlerin HTML sanilmasini onler;
+    //    white-space:pre ard arda gelen bosluklarin tek bosluga inmesini onler.
+    QString html = QStringLiteral("<span style=\"color:#334155;white-space:pre;\">");
+    if (start > 0)
+        html += ellipsis;
+    qsizetype cursor = start;
+    for (const auto &span : spans) {
+        const qsizetype spanStart = qMax(span.first, start);
+        const qsizetype spanEnd = qMin(span.first + span.second, end);
+        if (spanStart >= spanEnd)
+            continue;
+        html += text.mid(cursor, spanStart - cursor).toHtmlEscaped();
         html += QStringLiteral(
                     "<span style=\"background-color:#fde047;color:#713f12;font-weight:700;\">")
-              + text.mid(span.first, span.second).toHtmlEscaped()
+              + text.mid(spanStart, spanEnd - spanStart).toHtmlEscaped()
               + QStringLiteral("</span>");
-        cursor = span.first + span.second;
+        cursor = spanEnd;
     }
-    html += text.mid(cursor).toHtmlEscaped();
+    html += text.mid(cursor, end - cursor).toHtmlEscaped();
+    if (cutEnd)
+        html += ellipsis;
     html += QStringLiteral("</span>");
 
     QTextDocument doc;
@@ -59,9 +124,8 @@ void HighlightDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opt
     doc.setHtml(html);
     doc.setTextWidth(-1); // tek satir, sarma yok -- tablo satirlari tek satirlik
 
-    // 3) Hucrenin metin bolgesine, dikeyde ortalayarak ciz. Bolge disina tasan
-    //    kisim kirpilir (uzun mesajlarda tablo zaten yatay kaydiriyor/kesiyor).
-    const QRect textRect = style->subElementRect(QStyle::SE_ItemViewItemText, &opt, opt.widget);
+    // 4) Hucrenin metin bolgesine, dikeyde ortalayarak ciz. Olcumdeki kucuk
+    //    yuvarlama farklari icin bolge disi yine kirpilir.
     painter->save();
     painter->setClipRect(textRect);
     const qreal yOffset = (textRect.height() - doc.size().height()) / 2.0;
